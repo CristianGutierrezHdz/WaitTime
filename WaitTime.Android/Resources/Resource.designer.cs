542d93e068749eba8a821672392c251ec7de8a79
CristianGutierrezHdz <24gutierrez.cristian@gmail.com>
2023-05-03 23:23:51 -0600

    commit fer

--- WaitTime.Android/Resources/Resource.designer.cs
+++ WaitTime.Android/Resources/Resource.designer.cs
@@ -16733,25 +16733,28 @@ namespace WaitTime.Droid
 			public const int tik_tok_ek1 = 2131165350;
 			
 			// aapt resource value: 0x7F0700A7
-			public const int tooltip_frame_dark = 2131165351;
+			public const int tocar = 2131165351;
 			
 			// aapt resource value: 0x7F0700A8
-			public const int tooltip_frame_light = 2131165352;
+			public const int tooltip_frame_dark = 2131165352;
 			
 			// aapt resource value: 0x7F0700A9
-			public const int trazado_1 = 2131165353;
+			public const int tooltip_frame_light = 2131165353;
 			
 			// aapt resource value: 0x7F0700AA
-			public const int trazado_2 = 2131165354;
+			public const int trazado_1 = 2131165354;
 			
 			// aapt resource value: 0x7F0700AB
-			public const int trazado_3 = 2131165355;
+			public const int trazado_2 = 2131165355;
 			
 			// aapt resource value: 0x7F0700AC
-			public const int whatsapp = 2131165356;
+			public const int trazado_3 = 2131165356;
 			
 			// aapt resource value: 0x7F0700AD
-			public const int xamarin_logo = 2131165357;
+			public const int whatsapp = 2131165357;
+			
+			// aapt resource value: 0x7F0700AE
+			public const int xamarin_logo = 2131165358;
 			
 			static Drawable()
 			{
